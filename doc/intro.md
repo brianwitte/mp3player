03441afd78a0e28f7b0570ec5c8f1fd844f9b38d
bkz <bkz@dog>
2025-06-16 23:46:17 -0500

    first commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to mp3player
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/brianwitte/mp3player_test.clj
@@ -0,0 +1,7 @@
+(ns brianwitte.mp3player-test
+  (:require [clojure.test :refer :all]
+            [brianwitte.mp3player :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
